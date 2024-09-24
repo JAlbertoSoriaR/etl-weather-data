d54d08108926bb5e5a987fa24309c4471d06c88b
JAlbertoSoriaR <soriajs1312@gmail.com>
2024-09-23 19:26:51 -0600

    Implemented ETL pipeline for weather data extraction and loading into PostgreSQL

--- etl_weather_data/etl_script.py
+++ etl_weather_data/etl_script.py
@@ -1,4 +1,4 @@
-from etl_weather_data.etl_weather import extract_weather_data, transform_data
+from etl_weather_data.etl_weather import extract_weather_data, transform_data, load_data_to_postgresql
 
 def main():
     api_key = 'e8697333cd273b1093da4a10421f4c56'  
@@ -9,7 +9,12 @@ def main():
     if weather_data:
         transformed_data = transform_data(weather_data)  
 
-        print(transformed_data)
+        load_data_to_postgresql(
+            transformed_data,
+            db_name='weather_data',
+            user='weather_user',
+            password='weather'
+        )
 
 if __name__ == "__main__":
     main()
--- etl_weather_data/etl_weather.py
+++ etl_weather_data/etl_weather.py
@@ -2,6 +2,7 @@ import requests
 import json
 import pandas as pd
 from datetime import datetime
+import psycopg2 
 
 # Extract Weather Data
 def extract_weather_data(api_key, city):
@@ -28,11 +29,56 @@ def transform_data(weather_data):
     'city': [city_name],
     'temperature': [temperature],
     'humidity': [humidity],
-    'weather_description': [weather_desc],
+    'description': [weather_desc],
     'datetime': [datetime.now()]  # Record the timestamp
     }
 
     df = pd.DataFrame(data)
     return df
+
+# Load Data
+def load_data_to_postgresql(df, db_name, user, password, host='localhost', port='5432'):
+    try:
+        # Connect to PostgreSQL
+        conn = psycopg2.connect(
+            dbname=db_name,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+        cursor = conn.cursor()
+        
+        # Create table if it doesn't exist
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS weather (
+            city VARCHAR(50),
+            temperature FLOAT,
+            humidity INT,
+            description VARCHAR(100),
+            timestamp TIMESTAMP
+        );
+        ''')
+        
+        # Insert the weather data into the table
+        for index, row in df.iterrows():
+            insert_query = '''
+            INSERT INTO weather (city, temperature, humidity, description, timestamp)
+            VALUES (%s, %s, %s, %s, %s);
+            '''
+            cursor.execute(insert_query, (row['city'], row['temperature'], row['humidity'], row['description'], row['datetime']))
+
+        # Commit the transaction
+        conn.commit()
+        
+        print("Data loaded into PostgreSQL successfully!")
+    
+    except Exception as e:
+        print(f"Error: {e}")
+    
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
 
 

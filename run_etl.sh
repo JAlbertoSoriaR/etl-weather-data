d9c1e196835e38769e650faae76a48ec12dd4c5b
JAlbertoSoriaR <soriajs1312@gmail.com>
2024-09-24 18:56:07 -0600

    Updated environment variables

--- run_etl.sh
+++ run_etl.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# Navigate to the project directory
-cd /home/outis/Repositorios/etl-weather-data
+# Load environment variables from .env file
+source /home/outis/Repositorios/etl-weather-data/.env
 
-# Activate Poetry environment
-/home/outis/.local/bin/poetry run python etl_weather_data/etl_script.py
+# Check if PROJECT_DIR is set
+if [ -z "$PROJECT_DIR" ]; then
+  echo "Error: PROJECT_DIR is not set in the .env file."
+  exit 1
+fi
+
+# Navigate to the project directory
+cd "$PROJECT_DIR" || { echo "Error: Could not change to directory $PROJECT_DIR"; exit 1; }
+
+# Activate Poetry environment and run the script
+if [ -z "$POETRY_PATH" ]; then
+  echo "Error: POETRY_PATH is not set."
+  exit 1
+fi
+
+"$POETRY_PATH" run python etl_weather_data/etl_script.py
+
